5d18d61a9a3c20c9e56846e5a0d5a5f35e1aa1f9
김 태우 <connection0320@unipost.co.kr>
2026-03-24 10:44:29 +0900

    feat: React 초기 세팅 및 죄인 API 연동

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [sinners, setSinners] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/api/sinners")
+      .then((res) => res.json())
+      .then((data) => setSinners(data));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>림버스 컴퍼니</h1>
+      <ul>
+        {sinners.map((sinner) => (
+          <li key={sinner.id}>
+            {sinner.name} - {sinner.description}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
